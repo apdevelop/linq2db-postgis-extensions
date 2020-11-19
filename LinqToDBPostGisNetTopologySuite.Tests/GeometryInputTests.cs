c1590dec802977bfc9d1b13ec2c2f26af159be63
Andrey <andrey-vat@yandex.ru>
2020-11-19 21:13:45 +0300

    Implemented most Geometry Input methods from Other Formats

--- LinqToDBPostGisNetTopologySuite.Tests/GeometryInputTests.cs
+++ LinqToDBPostGisNetTopologySuite.Tests/GeometryInputTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 using LinqToDB;
 using NUnit.Framework;
@@ -43,6 +44,136 @@ namespace LinqToDBPostGisNetTopologySuite.Tests
                 db.TestGeometries.Value(g => g.Id, 3).Value(p => p.Geometry, () => null).Insert();
                 var srid3 = db.TestGeometries.Where(g => g.Id == 3).Select(g => g.Geometry.STSrId()).Single();
                 Assert.IsNull(srid3);
+            }
+        }
+
+        [Test]
+        public void TestSTGeomFromGeoHash()
+        {
+            using (var db = new PostGisTestDataConnection(TestDatabaseConnectionString))
+            {
+                const string geoHash = "9qqj7nmxncgyy4d0dbxqz0";
+
+                var wkt1 = db.Select(() => GeometryInput.STGeomFromGeoHash(geoHash).STAsText());
+                Assert.AreEqual("POLYGON((-115.172816 36.114646,-115.172816 36.114646,-115.172816 36.114646,-115.172816 36.114646,-115.172816 36.114646))", wkt1);
+
+                var wkt2 = db.Select(() => GeometryInput.STGeomFromGeoHash(geoHash, 4).STAsText());
+                Assert.AreEqual("POLYGON((-115.3125 36.03515625,-115.3125 36.2109375,-114.9609375 36.2109375,-114.9609375 36.03515625,-115.3125 36.03515625))", wkt2);
+
+                var wkt3 = db.Select(() => GeometryInput.STGeomFromGeoHash(geoHash, 10).STAsText());
+                Assert.AreEqual("POLYGON((-115.17282128334 36.1146408319473,-115.17282128334 36.1146461963654,-115.172810554504 36.1146461963654,-115.172810554504 36.1146408319473,-115.17282128334 36.1146408319473))", wkt3);
+
+                Assert.IsNull(db.Select(() => GeometryInput.STGeomFromGeoHash(null)));
+            }
+        }
+
+        [Test]
+        public void TestSTGeomFromGML()
+        {
+            using (var db = new PostGisTestDataConnection(TestDatabaseConnectionString))
+            {
+                var ewkt1 = db.Select(() => GeometryInput.STGeomFromGML("<gml:LineString srsName=\"EPSG:4269\"><gml:coordinates>-71.16028,42.258729 -71.160837,42.259112 -71.161143,42.25932</gml:coordinates></gml:LineString>").STAsEWKT());
+                Assert.AreEqual("SRID=4269;LINESTRING(-71.16028 42.258729,-71.160837 42.259112,-71.161143 42.25932)", ewkt1);
+
+                Assert.IsNull(db.Select(() => GeometryInput.STGeomFromGML(null)));
+            }
+        }
+
+        [Test]
+        public void TestSTGeomFromGeoJSON()
+        {
+            using (var db = new PostGisTestDataConnection(TestDatabaseConnectionString))
+            {
+                var wkt1 = db.Select(() => GeometryInput.STGeomFromGeoJSON("{\"type\":\"Point\",\"coordinates\":[-48.23456,20.12345]}").STAsText());
+                Assert.AreEqual("POINT(-48.23456 20.12345)", wkt1);
+
+                var wkt2 = db.Select(() => GeometryInput.STGeomFromGeoJSON("{\"type\":\"LineString\",\"coordinates\":[[1,2,3],[4,5,6],[7,8,9]]}").STAsText());
+                Assert.AreEqual("LINESTRING Z (1 2 3,4 5 6,7 8 9)", wkt2);
+
+                var wkt3 = db.Select(() => GeometryInput.STGeomFromGeoJSON(null).STAsText());
+                Assert.IsNull(wkt3);
+            }
+        }
+
+        [Test]
+        public void TestSTGeomFromKML()
+        {
+            using (var db = new PostGisTestDataConnection(TestDatabaseConnectionString))
+            {
+                var ewkt1 = db.Select(() => GeometryInput.STGeomFromKML("<LineString><coordinates>-71.1663, 42.2614 -71.1667, 42.2616</coordinates></LineString>").STAsEWKT());
+                Assert.AreEqual("SRID=4326;LINESTRING(-71.1663 42.2614,-71.1667 42.2616)", ewkt1);
+
+                var ewkt2 = db.Select(() => GeometryInput.STGeomFromKML(null).STAsEWKT());
+                Assert.IsNull(ewkt2);
+            }
+        }
+
+        [Test]
+        public void TestSTGeomFromTWKB()
+        {
+            using (var db = new PostGisTestDataConnection(TestDatabaseConnectionString))
+            {
+                var ewkt1 = db.Select(() => GeometryInput.STGeomFromTWKB(new byte[] { 0x62, 0x00, 0x02, 0xf7, 0xf4, 0x0d, 0xbc, 0xe4, 0x04, 0x01, 0x05 }).STAsEWKT());
+                Assert.AreEqual("LINESTRING(-113.98 39.198,-113.981 39.195)", ewkt1);
+
+                var ewkt2 = db.Select(() => GeometryInput.STGeomFromTWKB(null).STAsEWKT());
+                Assert.IsNull(ewkt2);
+            }
+        }
+
+        [Test]
+        public void TestSTLineFromEncodedPolyline()
+        {
+            using (var db = new PostGisTestDataConnection(TestDatabaseConnectionString))
+            {
+                const string ep1 = "_p~iF~ps|U_ulLnnqC_mqNvxq`@";
+                db.TestGeometries.Value(g => g.Id, 1).Value(p => p.Geometry, () => GeometryInput.STLineFromEncodedPolyline(ep1)).Insert();
+                db.TestGeometries.Value(g => g.Id, 2).Value(p => p.Geometry, () => GeometryInput.STLineFromEncodedPolyline(ep1, 6)).Insert();
+
+                var ewkt1 = db.TestGeometries.Where(g => g.Id == 1).Select(g => g.Geometry.STAsEWKT()).Single();
+                Assert.AreEqual("SRID=4326;LINESTRING(-120.2 38.5,-120.95 40.7,-126.453 43.252)", ewkt1);
+
+                var ewkt2 = db.TestGeometries.Where(g => g.Id == 2).Select(g => g.Geometry.STAsEWKT()).Single();
+                Assert.AreEqual("SRID=4326;LINESTRING(-12.02 3.85,-12.095 4.07,-12.6453 4.3252)", ewkt2);
+
+                db.TestGeometries.Value(g => g.Id, 3).Value(p => p.Geometry, () => GeometryInput.STLineFromEncodedPolyline(String.Empty)).Insert();
+                db.TestGeometries.Value(g => g.Id, 4).Value(p => p.Geometry, () => GeometryInput.STLineFromEncodedPolyline(null)).Insert();
+
+                var ewkt3 = db.TestGeometries.Where(g => g.Id == 3).Select(g => g.Geometry.STAsEWKT()).Single();
+                Assert.AreEqual("SRID=4326;LINESTRING EMPTY", ewkt3);
+
+                var ewkt4 = db.TestGeometries.Where(g => g.Id == 4).Select(g => g.Geometry.STAsEWKT()).Single();
+                Assert.IsNull(ewkt4);
+            }
+        }
+
+        [Test]
+        public void TestSTPointFromGeoHash()
+        {
+            using (var db = new PostGisTestDataConnection(TestDatabaseConnectionString))
+            {
+                const string geohash1 = "9qqj7nmxncgyy4d0dbxqz0";
+                db.TestGeometries.Value(g => g.Id, 1).Value(p => p.Geometry, () => GeometryInput.STPointFromGeoHash(geohash1)).Insert();
+                db.TestGeometries.Value(g => g.Id, 2).Value(p => p.Geometry, () => GeometryInput.STPointFromGeoHash(geohash1, 4)).Insert();
+                db.TestGeometries.Value(g => g.Id, 3).Value(p => p.Geometry, () => GeometryInput.STPointFromGeoHash(geohash1, 10)).Insert();
+
+                var wkt1 = db.TestGeometries.Where(g => g.Id == 1).Select(g => g.Geometry.STAsText()).Single();
+                Assert.AreEqual("POINT(-115.172816 36.114646)", wkt1);
+
+                var wkt2 = db.TestGeometries.Where(g => g.Id == 2).Select(g => g.Geometry.STAsText()).Single();
+                Assert.AreEqual("POINT(-115.13671875 36.123046875)", wkt2);
+
+                var wkt3 = db.TestGeometries.Where(g => g.Id == 3).Select(g => g.Geometry.STAsText()).Single();
+                Assert.AreEqual("POINT(-115.172815918922 36.1146435141563)", wkt3);
+
+                db.TestGeometries.Value(g => g.Id, 4).Value(p => p.Geometry, () => GeometryInput.STPointFromGeoHash(String.Empty)).Insert();
+                db.TestGeometries.Value(g => g.Id, 5).Value(p => p.Geometry, () => GeometryInput.STPointFromGeoHash(null)).Insert();
+
+                var wkt4 = db.TestGeometries.Where(g => g.Id == 4).Select(g => g.Geometry.STAsText()).Single();
+                Assert.AreEqual("POINT(0 0)", wkt4);
+
+                var wkt5 = db.TestGeometries.Where(g => g.Id == 5).Select(g => g.Geometry.STAsText()).Single();
+                Assert.IsNull(wkt5);
             }
         }
     }
--- LinqToDBPostGisNetTopologySuite/GeometryInput.cs
+++ LinqToDBPostGisNetTopologySuite/GeometryInput.cs
@@ -14,6 +14,8 @@ namespace LinqToDBPostGisNetTopologySuite
     /// </remarks>
     public static class GeometryInput
     {
+        // TODO: Complete 8.8.1. Well-Known Text (WKT)
+
         /// <summary>
         /// Constructs geometry from the Extended Well-Known Text (EWKT) representation.
         /// </summary>
@@ -56,5 +58,202 @@ namespace LinqToDBPostGisNetTopologySuite
         {
             throw new InvalidOperationException();
         }
+
+        // TODO: Implement 8.8.2. Well-Known Binary (WKB)
+
+        #region 8.8.3. Other Formats
+
+        // TODO: ST_Box2dFromGeoHash return type NTS mapping?
+
+        /// <summary>
+        /// Constructs Polygon geometry from given GeoHash string.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_GeomFromGeoHash.html
+        /// </remarks>
+        /// <param name="geoHash">GeoHash string</param>
+        /// <returns>Polygon representing the GeoHash bounds</returns>
+        [Sql.Function("ST_GeomFromGeoHash", ServerSideOnly = true)]
+        public static NTSG STGeomFromGeoHash(string geoHash)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Constructs Polygon geometry from given GeoHash string.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_GeomFromGeoHash.html
+        /// </remarks>
+        /// <param name="geoHash">GeoHash string</param>
+        /// <param name="precision">Number of characters to use from GeoHash</param>
+        /// <returns>Polygon representing the GeoHash bounds</returns>
+        [Sql.Function("ST_GeomFromGeoHash", ServerSideOnly = true)]
+        public static NTSG STGeomFromGeoHash(string geoHash, int precision)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Constructs geometry from OGC GML representation.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_GeomFromGML.html
+        /// </remarks>
+        /// <param name="gml">GML string</param>
+        /// <returns>Geometry</returns>
+        [Sql.Function("ST_GeomFromGML", ServerSideOnly = true)]
+        public static NTSG STGeomFromGML(string gml)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Constructs geometry from OGC GML representation with given SRID.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_GeomFromGML.html
+        /// </remarks>
+        /// <param name="gml">GML string</param>
+        /// <param name="srid">Spatial Reference System Identifier for geometry</param>
+        /// <returns>Geometry</returns>
+        [Sql.Function("ST_GeomFromGML", ServerSideOnly = true)]
+        public static NTSG STGeomFromGML(string gml, int srid)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Constructs geometry from GeoJSON representation.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_GeomFromGeoJSON.html
+        /// </remarks>
+        /// <param name="geoJson">GeoJSON string</param>
+        /// <returns>Geometry</returns>
+        [Sql.Function("ST_GeomFromGeoJSON", ServerSideOnly = true)]
+        public static NTSG STGeomFromGeoJSON(string geoJson)
+        {
+            throw new InvalidOperationException();
+        }
+
+        // TODO: ? json / jsonb ?
+
+        /// <summary>
+        /// Constructs geometry from OGC KML representation.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_GeomFromKML.html
+        /// </remarks>
+        /// <param name="kml">KML string</param>
+        /// <returns>Geometry</returns>
+        [Sql.Function("ST_GeomFromKML", ServerSideOnly = true)]
+        public static NTSG STGeomFromKML(string kml)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Constructs geometry from Tiny Well-Known Binary (TWKB) representation.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_GeomFromTWKB.html
+        /// </remarks>
+        /// <param name="twkb">Tiny Well-Known Binary (TWKB)</param>
+        /// <returns>Geometry</returns>
+        [Sql.Function("ST_GeomFromTWKB", ServerSideOnly = true)]
+        public static NTSG STGeomFromTWKB(byte[] twkb)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Constructs geometry from OGC GML representation (Alias for STGeomFromGML).
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_GMLToSQL.html
+        /// </remarks>
+        /// <param name="gml">GML string</param>
+        /// <returns>Geometry</returns>
+        [Sql.Function("ST_GMLToSQL", ServerSideOnly = true)]
+        public static NTSG STGMLToSQL(string gml)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Constructs geometry from OGC GML representation with given SRID (Alias for STGeomFromGML).
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_GMLToSQL.html
+        /// </remarks>
+        /// <param name="gml">GML string</param>
+        /// <param name="srid">Spatial Reference System Identifier for geometry</param>
+        /// <returns>Geometry</returns>
+        [Sql.Function("ST_GMLToSQL", ServerSideOnly = true)]
+        public static NTSG STGMLToSQL(string gml, int srid)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Constructs LineString geometry from given Encoded Polyline string.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_LineFromEncodedPolyline.html
+        /// </remarks>
+        /// <param name=" polyline">Encoded Polyline string</param>
+        /// <returns>LineString geometry</returns>
+        [Sql.Function("ST_LineFromEncodedPolyline", ServerSideOnly = true)]
+        public static NTSG STLineFromEncodedPolyline(string polyline)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Constructs LineString geometry from given Encoded Polyline string.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_LineFromEncodedPolyline.html
+        /// </remarks>
+        /// <param name=" polyline">Encoded Polyline string</param>
+        /// <param name="precision">Number of characters to use from Encoded Polyline string</param>
+        /// <returns>LineString geometry</returns>
+        [Sql.Function("ST_LineFromEncodedPolyline", ServerSideOnly = true)]
+        public static NTSG STLineFromEncodedPolyline(string polyline, int precision)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Constructs Point geometry from given GeoHash string.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_PointFromGeoHash.html
+        /// </remarks>
+        /// <param name="geoHash">GeoHash string</param>
+        /// <returns>Geometry (center point of GeoHash)</returns>
+        [Sql.Function("ST_PointFromGeoHash", ServerSideOnly = true)]
+        public static NTSG STPointFromGeoHash(string geoHash)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Constructs Point geometry from given GeoHash string.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_PointFromGeoHash.html
+        /// </remarks>
+        /// <param name="geoHash">GeoHash string</param>
+        /// <param name="precision">Number of characters to use from GeoHash</param>
+        /// <returns>Geometry (center point of GeoHash)</returns>
+        [Sql.Function("ST_PointFromGeoHash", ServerSideOnly = true)]
+        public static NTSG STPointFromGeoHash(string geoHash, int precision)
+        {
+            throw new InvalidOperationException();
+        }
+
+        #endregion
     }
 }
