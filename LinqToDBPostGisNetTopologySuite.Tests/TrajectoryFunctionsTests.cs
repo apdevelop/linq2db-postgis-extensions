dd49e58009efc225c73be7d139b31dd7772c154f
Andrey <andrey-vat@yandex.ru>
2021-01-12 12:21:19 +0300

    Implemented Trajectory Functions methods with tests

---
+++ LinqToDBPostGisNetTopologySuite.Tests/TrajectoryFunctionsTests.cs
@@ -0,0 +1,116 @@
+﻿using System.Linq;
+
+using LinqToDB;
+using NUnit.Framework;
+
+namespace LinqToDBPostGisNetTopologySuite.Tests
+{
+    [TestFixture]
+    class TrajectoryFunctionsTests : TestsBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+            using (var db = new PostGisTestDataConnection(TestDatabaseConnectionString))
+            {
+                db.TestGeometries.Delete();
+            }
+        }
+
+        [Test]
+        public void TestSTIsValidTrajectory()
+        {
+            using (var db = new PostGisTestDataConnection(TestDatabaseConnectionString))
+            {
+                db.TestGeometries
+                    .Value(g => g.Id, 1)
+                    .Value(p => p.Geometry, () =>
+                         GeometryConstructors.STMakeLine(
+                             GeometryConstructors.STMakePointM(0, 0, 1),
+                             GeometryConstructors.STMakePointM(0, 1, 2)))
+                    .Insert();
+
+                db.TestGeometries
+                    .Value(g => g.Id, 2)
+                    .Value(p => p.Geometry, () =>
+                         GeometryConstructors.STMakeLine(
+                             GeometryConstructors.STMakePointM(0, 0, 1),
+                             GeometryConstructors.STMakePointM(0, 1, 0)))
+                    .Insert();
+
+                db.TestGeometries.Value(g => g.Id, 3).Value(p => p.Geometry, () => null).Insert();
+
+                Assert.IsTrue(db.TestGeometries.Where(g => g.Id == 1).Select(g => g.Geometry.STIsValidTrajectory()).Single());
+                Assert.IsFalse(db.TestGeometries.Where(g => g.Id == 2).Select(g => g.Geometry.STIsValidTrajectory()).Single());
+                Assert.IsNull(db.TestGeometries.Where(g => g.Id == 3).Select(g => g.Geometry.STIsValidTrajectory()).Single());
+            }
+        }
+
+        [Test]
+        public void TestSTClosestPointOfApproach()
+        {
+            using (var db = new PostGisTestDataConnection(TestDatabaseConnectionString))
+            {
+                const string wkt1 = "LINESTRING ZM (0 0 0 1432623600,10 0 5 1432627200)";
+                db.TestGeometries.Value(g => g.Id, 1).Value(p => p.Geometry, () => GeometryInput.STGeomFromText(wkt1)).Insert();
+
+                const string wkt2 = "LINESTRING ZM (0 2 10 1432623600,12 1 2 1432627200)";
+                db.TestGeometries.Value(g => g.Id, 2).Value(p => p.Geometry, () => GeometryInput.STGeomFromText(wkt2)).Insert();
+
+                db.TestGeometries.Value(g => g.Id, 3).Value(p => p.Geometry, () => null).Insert();
+
+                Assert.AreEqual(1432626331.03448,
+                    db.TestGeometries.Where(g => g.Id == 1)
+                    .Select(g => g.Geometry.STClosestPointOfApproach(db.TestGeometries.Where(g2 => g2.Id == 2).Single().Geometry))
+                    .Single(),
+                    1.0E-3);
+
+                Assert.IsNull(db.TestGeometries.Where(g => g.Id == 3).Select(g => g.Geometry.STClosestPointOfApproach(null)).Single());
+            }
+        }
+
+        [Test]
+        public void TestSTDistanceCPA()
+        {
+            using (var db = new PostGisTestDataConnection(TestDatabaseConnectionString))
+            {
+                const string wkt1 = "LINESTRING ZM (0 0 0 1432623600,10 0 5 1432627200)";
+                db.TestGeometries.Value(g => g.Id, 1).Value(p => p.Geometry, () => GeometryInput.STGeomFromText(wkt1)).Insert();
+
+                const string wkt2 = "LINESTRING ZM (0 2 10 1432623600,12 1 2 1432627200)";
+                db.TestGeometries.Value(g => g.Id, 2).Value(p => p.Geometry, () => GeometryInput.STGeomFromText(wkt2)).Insert();
+
+                db.TestGeometries.Value(g => g.Id, 3).Value(p => p.Geometry, () => null).Insert();
+
+                Assert.AreEqual(1.96521473776207,
+                    db.TestGeometries.Where(g => g.Id == 1)
+                    .Select(g => g.Geometry.STDistanceCPA(db.TestGeometries.Where(g2 => g2.Id == 2).Single().Geometry))
+                    .Single(),
+                    1.0E-8);
+
+                Assert.IsNull(db.TestGeometries.Where(g => g.Id == 3).Select(g => g.Geometry.STDistanceCPA(null)).Single());
+            }
+        }
+
+        [Test]
+        public void TestSTCPAWithin()
+        {
+            using (var db = new PostGisTestDataConnection(TestDatabaseConnectionString))
+            {
+                const string wkt1 = "LINESTRING ZM (0 0 0 1432623600,10 0 5 1432627200)";
+                db.TestGeometries.Value(g => g.Id, 1).Value(p => p.Geometry, () => GeometryInput.STGeomFromText(wkt1)).Insert();
+
+                const string wkt2 = "LINESTRING ZM (0 2 10 1432623600,12 1 2 1432627200)";
+                db.TestGeometries.Value(g => g.Id, 2).Value(p => p.Geometry, () => GeometryInput.STGeomFromText(wkt2)).Insert();
+
+                db.TestGeometries.Value(g => g.Id, 3).Value(p => p.Geometry, () => null).Insert();
+
+                Assert.IsTrue(db.TestGeometries.Where(g => g.Id == 1)
+                    .Select(g => g.Geometry.STCPAWithin(db.TestGeometries.Where(g2 => g2.Id == 2).Single().Geometry, 2))
+                    .Single());
+
+                Assert.IsNull(db.TestGeometries.Where(g => g.Id == 3).Select(g => g.Geometry.STCPAWithin(null, 2)).Single());
+            }
+        }
+    }
+}
---
+++ LinqToDBPostGisNetTopologySuite/TrajectoryFunctions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+using LinqToDB;
+
+using NTSG = NetTopologySuite.Geometries.Geometry;
+
+namespace LinqToDBPostGisNetTopologySuite
+{
+    /// <summary>
+    /// Trajectory Functions
+    /// </summary>
+    /// <remarks>
+    /// 8.18. Trajectory Functions https://postgis.net/docs/manual-3.0/reference.html#Temporal
+    /// </remarks>
+    public static class TrajectoryFunctions
+    {
+        /// <summary>
+        /// Tests if input geometry represents valid trajectory - LINESTRING with measures (M values), which increase from each vertex to the next.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_IsValidTrajectory.html
+        /// </remarks>
+        /// <param name="geometry">Input geometry</param>
+        /// <returns>True if input geometry is a valid trajectory</returns>
+        [Sql.Function("ST_IsValidTrajectory", ServerSideOnly = true)]
+        public static bool? STIsValidTrajectory(this NTSG geometry)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Returns the smallest measure at which points interpolated along the given trajectories are at the smallest distance.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_ClosestPointOfApproach.html
+        /// </remarks>
+        /// <param name="track1">Input geometry for trajectory 1</param>
+        /// <param name="track2">Input geometry for trajectory 2</param>
+        /// <returns>Smallest measure at the smallest distance</returns>
+        [Sql.Function("ST_ClosestPointOfApproach", ServerSideOnly = true)]
+        public static double? STClosestPointOfApproach(this NTSG track1, NTSG track2)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Returns the minimum distance two input geometries have ever been each other.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_DistanceCPA.html
+        /// </remarks>
+        /// <param name="track1">Input geometry for trajectory 1</param>
+        /// <param name="track2">Input geometry for trajectory 2</param>
+        /// <returns>Minimum distance</returns>
+        [Sql.Function("ST_DistanceCPA", ServerSideOnly = true)]
+        public static double? STDistanceCPA(this NTSG track1, NTSG track2)
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Checks whether two input geometries have ever been within given maximum distance.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/manual-3.0/ST_CPAWithin.html
+        /// </remarks>
+        /// <param name="track1">Input geometry for trajectory 1</param>
+        /// <param name="track2">Input geometry for trajectory 2</param>
+        /// <param name="maxDistance">Maximum distance</param>
+        /// <returns>True, if two input geometries have ever been within given maximum distance</returns>
+        [Sql.Function("ST_CPAWithin", ServerSideOnly = true)]
+        public static bool? STCPAWithin(this NTSG track1, NTSG track2, double maxDistance)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
