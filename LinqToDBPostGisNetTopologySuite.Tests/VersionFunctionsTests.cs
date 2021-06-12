a066a727236c5773e6474b63063fd943d6cb895f
Andrey <andrey-vat@yandex.ru>
2021-06-12 21:24:55 +0300

    Implemented full set of Version Functions

--- LinqToDBPostGisNetTopologySuite.Tests/VersionFunctionsTests.cs
+++ LinqToDBPostGisNetTopologySuite.Tests/VersionFunctionsTests.cs
@@ -21,6 +21,42 @@ namespace LinqToDBPostGisNetTopologySuite.Tests
                 Console.WriteLine(new String('-', 40));
                 Console.WriteLine(fullVersion);
                 Console.WriteLine(new String('-', 40));
+
+                var geosVersion = db.Select(() => VersionFunctions.PostGISGEOSVersion());
+                Console.WriteLine($"GEOS={geosVersion}");
+
+                var liblwgeomVersion = db.Select(() => VersionFunctions.PostGISLiblwgeomVersion());
+                Console.WriteLine($"liblwgeom={liblwgeomVersion}");
+
+                var libXmlVersion = db.Select(() => VersionFunctions.PostGISLibXMLVersion());
+                Console.WriteLine($"LibXML={libXmlVersion}");
+
+                var libBuildDate = db.Select(() => VersionFunctions.PostGISLibBuildDate());
+                Console.WriteLine($"PostGIS lib build date={libBuildDate}");
+
+                var libVersion = db.Select(() => VersionFunctions.PostGISLibVersion());
+                Console.WriteLine($"PostGIS lib version={libVersion}");
+
+                var projVersion = db.Select(() => VersionFunctions.PostGISPROJVersion());
+                Console.WriteLine($"PROJ4={projVersion}");
+
+                var currentVersion = new Version(db.Select(() => VersionFunctions.PostGISLibVersion()));
+                if (currentVersion >= new Version("3.0"))
+                {
+                    var wagyuVersion = db.Select(() => VersionFunctions.PostGISWagyuVersion());
+                    Console.WriteLine($"Wagyu={wagyuVersion}");
+                }
+
+                var scriptsBuildDate = db.Select(() => VersionFunctions.PostGISScriptsBuildDate());
+                Console.WriteLine($"PostGIS scripts build date={scriptsBuildDate}");
+
+                var scripts = db.Select(() => VersionFunctions.PostGISScriptsInstalled());
+                Console.WriteLine($"PostGIS scripts={scripts}");
+
+                var postGisVersion = db.Select(() => VersionFunctions.PostGISVersion());
+                Console.WriteLine($"PostGIS version={postGisVersion}");
+
+                Console.WriteLine(new String('-', 40));
             }
         }
     }
--- LinqToDBPostGisNetTopologySuite/VersionFunctions.cs
+++ LinqToDBPostGisNetTopologySuite/VersionFunctions.cs
@@ -43,6 +43,51 @@ namespace LinqToDBPostGisNetTopologySuite
         }
 
         /// <summary>
+        /// Returns the version number of the liblwgeom library.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/PostGIS_Liblwgeom_Version.html
+        /// </remarks>
+        /// <returns>
+        /// Version number of the liblwgeom library.
+        /// </returns>
+        [Sql.Function("PostGIS_Liblwgeom_Version", ServerSideOnly = true)]
+        public static string PostGISLiblwgeomVersion()
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Returns the version number of the libxml library.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/PostGIS_LibXML_Version.html
+        /// </remarks>
+        /// <returns>
+        /// Version number of the libxml library.
+        /// </returns>
+        [Sql.Function("PostGIS_LibXML_Version", ServerSideOnly = true)]
+        public static string PostGISLibXMLVersion()
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Returns the build date of the PostGIS library.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/PostGIS_Lib_Build_Date.html
+        /// </remarks>
+        /// <returns>
+        /// Build date of the PostGIS library.
+        /// </returns>
+        [Sql.Function("PostGIS_Lib_Build_Date", ServerSideOnly = true)]
+        public static string PostGISLibBuildDate()
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
         /// Returns the version number of the PostGIS library.
         /// </summary>
         /// <remarks>
@@ -53,6 +98,66 @@ namespace LinqToDBPostGisNetTopologySuite
         /// </returns>
         [Sql.Function("PostGIS_Lib_Version", ServerSideOnly = true)]
         public static string PostGISLibVersion()
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Returns the version number of the PROJ4 library, or null if PROJ4 support is not enabled.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/PostGIS_PROJ_Version.html
+        /// </remarks>
+        /// <returns>
+        /// Version number of the PROJ4 library.
+        /// </returns>
+        [Sql.Function("PostGIS_PROJ_Version", ServerSideOnly = true)]
+        public static string PostGISPROJVersion()
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Returns the version number of the internal Wagyu library, or null if Wagyu support is not enabled.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/PostGIS_Wagyu_Version.html
+        /// </remarks>
+        /// <returns>
+        /// Version number of the Wagyu library.
+        /// </returns>
+        [Sql.Function("PostGIS_Wagyu_Version", ServerSideOnly = true)]
+        public static string PostGISWagyuVersion()
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Returns the build date of the PostGIS scripts.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/PostGIS_Scripts_Build_Date.html
+        /// </remarks>
+        /// <returns>
+        /// Build date of the PostGIS scripts.
+        /// </returns>
+        [Sql.Function("PostGIS_Scripts_Build_Date", ServerSideOnly = true)]
+        public static string PostGISScriptsBuildDate()
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Returns version of the PostGIS scripts installed in this database.
+        /// </summary>
+        /// <remarks>
+        /// See https://postgis.net/docs/PostGIS_Scripts_Installed.html
+        /// </remarks>
+        /// <returns>
+        /// Version of the PostGIS scripts.
+        /// </returns>
+        [Sql.Function("PostGIS_Scripts_Installed", ServerSideOnly = true)]
+        public static string PostGISScriptsInstalled()
         {
             throw new InvalidOperationException();
         }
